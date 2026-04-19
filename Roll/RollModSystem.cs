d7c9cd068eac194e1ff831f08d6ddd22ffb5905f
Marat Zaripov <m.zaripov@kefirgames.com>
2026-04-19 19:29:00 +0300

    add: project

---
+++ Roll/RollModSystem.cs
@@ -0,0 +1,35 @@
+﻿using Vintagestory.API.Common;
+using Vintagestory.API.Server;
+
+namespace Roll;
+
+public class RollModSystem : ModSystem
+{
+    private ICoreServerAPI sapi;
+    private Random rand = new Random();
+    
+    public override bool ShouldLoad(EnumAppSide forSide) => forSide == EnumAppSide.Server;
+    
+    public override void StartServerSide(ICoreServerAPI api)
+    {
+        sapi = api;
+
+        sapi.ChatCommands.Create("roll")
+            .WithDescription("Get random value")
+            .RequiresPrivilege(Privilege.chat)
+            .HandleWith(OnCommandDice);
+    }
+    
+    private TextCommandResult OnCommandDice(TextCommandCallingArgs args)
+    {
+        var player = args.Caller.Player as IServerPlayer;
+        if (player == null)
+            return TextCommandResult.Error("Player is null");
+        
+        var randomNumber = rand.Next(1, 101);
+        var message = $"{player.PlayerName} Random number: {randomNumber}";
+        player.SendMessage(args.Caller.FromChatGroupId, message, EnumChatType.Notification);
+
+        return TextCommandResult.Success();
+    }
+}
